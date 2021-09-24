dd437647f91c87c120b99c7c7c8536e2de741fb3
Pablo <email@gmail.com>
2021-09-23 21:47:23 -0600

    Trabajando en el analizador lexico

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -1,7 +1,8 @@
 from Token import Token
 from Error import Error
 from prettytable import PrettyTable
-
+import re
+TITULO="Pokeball";
 
 class AnalizadorLexico:
 
@@ -23,80 +24,181 @@ class AnalizadorLexico:
 
     def estadoA(self, caracter):
         if caracter == ';':
-            self.agregar_token(caracter, 'puntoycoma', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "L"
         elif caracter == ',':
-            self.agregar_token(caracter, 'coma', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "K"
         elif caracter == '[':
-            self.agregar_token(caracter, 'corchete-izquierdo', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "I"
         elif caracter == ']':
-            self.agregar_token(caracter, 'corchetederecho', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "J"
         elif caracter == '{':
-            self.agregar_token(caracter, 'llave-izquierda', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "G"
         elif caracter == '}':
-            self.agregar_token(caracter, 'llave-derecha', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "H"
         elif caracter == '=':
-            self.agregar_token(caracter, 'igual', self.linea, self.columna)
             self.buffer += caracter
             self.columna += 1
+            self.estado = "M"
+        elif caracter.isdigit():
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "D"
+        elif caracter == '#':
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "E"
+        elif caracter == '"':
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "B"
+        elif caracter == '@':
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "F"
+        elif caracter.isalpha():
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "C"
         elif caracter == '\n':
             self.linea += 1
             self.columna = 1
         elif caracter in ['\t', ' ']:
             self.columna += 1
+        else:
+            self.buffer += caracter
+            self.agregar_error(self.buffer, self.linea, self.columna)
+            self.buffer = ''
+            self.columna += 1
 
-    def estadoB(self):
-        pass
+    def estadoB(self, caracter):
+        if caracter != "\"":
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "N"
+        else:
+            self.estado = "A"
+            self.agregar_error(self.buffer, self.linea, self.columna)
+            self.i -=1
+            self.buffer = ''
 
-    def estadoC(self):
-        pass
 
-    def estadoD(self):
-        pass
+    def estadoC(self, caracter):
+        if caracter.isalpha():
+            self.buffer += caracter
+            self.columna += 1
+        else:
+            if re.search("titulo|ancho|alto|filas|columnas|celdas|true|false|filtros|mirrorx|mirrory|doublemirror", self.buffer.lower()):
+                self.agregar_token(self.buffer, 'reservada', self.linea, self.columna)
+                self.estado = "A"
+                self.i -= 1
+            else:
+                self.agregar_error(self.buffer, self.linea, self.columna)
+                self.estado = "A"
+                self.i -= 1
+                self.buffer = ''
 
-    def estadoE(self):
-        pass
+    def estadoD(self, caracter):
+        if caracter.isdigit():
+            self.buffer += caracter
+            self.columna += 1
+        else:
+            self.agregar_token(self.buffer, 'digito', self.linea, self.columna)
+            self.estado = "A"
+            self.i -= 1
 
-    def estadoF(self):
-        pass
+    def estadoE(self, caracter):
+        if caracter.isalpha() or caracter.isdigit():
+            self.buffer += caracter
+            self.columna += 1
+            self.estado = "O"
+        else:
+            self.agregar_error(self.buffer, self.linea, self.columna)
+            self.estado = "A"
+            self.buffer = ''
+            self.i -= 1
 
-    def estadoG(self):
-        pass
+    def estadoO(self, caracter):
+        if caracter.isalpha() or caracter.isdigit():
+            self.buffer += caracter
+            self.columna += 1
+        else:
+            self.agregar_token(self.buffer, 'color hex', self.linea, self.columna)
+            self.estado = "A"
+            self.i -= 1
 
-    def estadoH(self):
-        pass
+    def estadoF(self, caracter):
+        if caracter == "@":
+            self.buffer += caracter
+            self.columna += 1
+        else:
+            if len(self.buffer) == 4:
+                self.agregar_token(self.buffer, 'separador', self.linea, self.columna)
 
-    def estadoI(self):
-        pass
+            else:
+                self.agregar_error(self.buffer, self.linea, self.columna)
+                self.buffer = ''
+            self.estado = "A"
+            self.i -= 1
 
-    def estadoJ(self):
-        pass
+    def estadoG(self, caracter):
+        self.agregar_token(self.buffer, 'llave izquierda', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
 
-    def estadoK(self):
-        pass
+    def estadoH(self, caracter):
+        self.agregar_token(self.buffer, 'llave derecha', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
 
-    def estadoL(self):
-        pass
+    def estadoI(self, caracter):
+        self.agregar_token(self.buffer, 'corchete izquierdo', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
 
-    def estadoM(self):
-        pass
+    def estadoJ(self, caracter):
+        self.agregar_token(self.buffer, 'corchete derecho', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
 
-    def estadoN(self):
-        pass
+    def estadoK(self, caracter):
+        self.agregar_token(self.buffer, 'coma', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
 
-    def estadoO(self):
-        pass
+    def estadoL(self, caracter):
+        self.agregar_token(self.buffer, 'punto y coma', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
+
+    def estadoM(self, caracter):
+        self.agregar_token(self.buffer, 'simbolo =', self.linea, self.columna)
+        self.i -= 1
+        self.estado = "A"
+
+    def estadoN(self, caracter):
+        if caracter != "\"":
+            self.buffer += caracter
+            self.columna += 1
+        else:
+            self.buffer += caracter
+            self.agregar_token(self.buffer, 'identificador', self.linea, self.columna)
+            self.estado = "P"
+
+    def estadoP(self, caracter):
+        self.estado = "A"
+        self.i -= 1
 
     def analizar(self, cadena):
         self.listaTokens = []
@@ -107,34 +209,38 @@ class AnalizadorLexico:
             if self.estado == "A":
                 self.estadoA(cadena[self.i])
             elif self.estado == "B":
-                self.estadoA(cadena[self.i])
+                self.estadoB(cadena[self.i])
             elif self.estado == "C":
-                self.estadoA(cadena[self.i])
+                self.estadoC(cadena[self.i])
             elif self.estado == "D":
-                self.estadoA(cadena[self.i])
+                self.estadoD(cadena[self.i])
             elif self.estado == "E":
-                self.estadoA(cadena[self.i])
+                self.estadoE(cadena[self.i])
             elif self.estado == "F":
-                self.estadoA(cadena[self.i])
+                self.estadoF(cadena[self.i])
             elif self.estado == "G":
-                self.estadoA(cadena[self.i])
+                self.estadoG(cadena[self.i])
             elif self.estado == "H":
-                self.estadoA(cadena[self.i])
+                self.estadoH(cadena[self.i])
             elif self.estado == "I":
-                self.estadoA(cadena[self.i])
+                self.estadoI(cadena[self.i])
             elif self.estado == "J":
-                self.estadoA(cadena[self.i])
+                self.estadoJ(cadena[self.i])
             elif self.estado == "K":
-                self.estadoA(cadena[self.i])
+                self.estadoK(cadena[self.i])
             elif self.estado == "L":
-                self.estadoA(cadena[self.i])
+                self.estadoL(cadena[self.i])
             elif self.estado == "M":
-                self.estadoA(cadena[self.i])
+                self.estadoM(cadena[self.i])
             elif self.estado == "N":
-                self.estadoA(cadena[self.i])
+                self.estadoN(cadena[self.i])
             elif self.estado == "O":
-                self.estadoA(cadena[self.i])
+                self.estadoO(cadena[self.i])
+            elif self.estado == "P":
+                self.estadoP(cadena[self.i])
             self.i += 1
+        self.impErrores()
+        self.impTokens()
 
     # imprimeTokens
     def impTokens(self):
